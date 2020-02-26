782b576a3cbaf3186a748e36ae898da57ead784f
Prakash Choudhary <om.pcprakash@gmail.com>
2020-02-27 02:47:52 +0530

    Multiple lifts and floors

--- scripts/liftScripts.js
+++ scripts/liftScripts.js
@@ -1,19 +1,19 @@
 let isLiftWorking = false;
 let totalFloor = 2
-let liftPresentFloor = 0;
+let liftPresentFloor = [];
 
 //prototype used to add element of elemet
 Element.prototype.getElementById = function (id) {
- return document.getElementById(id);
+    return document.getElementById(id);
 }
 
 //Ofset values function
 
 function offset(el) {
- var rect = el.getBoundingClientRect(),
-  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
- return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
 
 // var div11 = document.getElementById("floor0");
@@ -27,98 +27,102 @@ function offset(el) {
 
 let positionOfLiftTop = [];
 
+
 let adjustTop = offset(document.getElementById('floor0')).top;
+
 for (let i = 0; i < noOfLifts; i++) {
- let x = document.getElementById('liftSetup' + i);
- let positionOfLift = offset(x);
+    liftPresentFloor = 0;
+    let x = document.getElementById('liftSetup' + i);
+    let positionOfLift = offset(x);
 
- console.log(x, positionOfLift, i);
- positionOfLiftTop[i] = positionOfLift.top + adjustTop - 45;
- let positionOfLiftLeft = positionOfLift.left + 40;
+    console.log(x, positionOfLift, i);
+    positionOfLiftTop[i] = positionOfLift.top + adjustTop - 45;
+    let positionOfLiftLeft = positionOfLift.left + 40;
 
- let lift = document.getElementById('lift' + i);
- lift.style.top = positionOfLiftTop[i] + 'px';
- lift.style.left = positionOfLiftLeft + 'px';
+    let lift = document.getElementById('lift' + i);
+    lift.style.top = positionOfLiftTop[i] + 'px';
+    lift.style.left = positionOfLiftLeft + 'px';
 }
-let positionOfLiftFloor = 0;
 
 function liftWorkingStatus() {
- isLiftWorking = true;
+    isLiftWorking = true;
 }
 
 function moveLiftUp(floorNumber, liftNumber) {
- let lift1 = document.getElementById(liftNumber);
- let floorA = offset(document.getElementById("floor" + floorNumber));
- let top = floorA.top;
- let left = floorA.left;
- console.log(top, left);
 
- liftNumber = liftNumber.match(/\d/g);
- liftNumber = liftNumber.join("");
+    let lift1 = document.getElementById(liftNumber);
+    liftNumber = liftNumber.match(/\d/g);
+    liftNumber = liftNumber.join("");
+    let x = document.getElementById('liftSetup' + liftNumber);
+    let adjustTop = offset(x).top;
+    let floorA = offset(document.getElementById("floor" + floorNumber));
+    let top = floorA.top + adjustTop - 45;
+    let left = floorA.left;
+    console.log(top, left);
 
 
- if (liftPresentFloor != floorNumber) {
-  let id = setInterval(frameDown, 10);
-  function frameDown() {
-   if (positionOfLiftTop[liftNumber] >= top) {
-    clearInterval(id);
-   } else {
-    positionOfLiftTop[liftNumber]++;
-    lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
-   }
-  }
+    if (liftPresentFloor[liftNumber] != floorNumber) {
+        let id = setInterval(frameDown, 5);
+        function frameDown() {
+            if (positionOfLiftTop[liftNumber] >= top) {
+                clearInterval(id);
+            } else {
+                positionOfLiftTop[liftNumber]++;
+                lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
+            }
+        }
 
- }
+    }
 
- let id = setInterval(frame, 10);
- function frame() {
-  if (positionOfLiftTop[liftNumber] <= top) {
-   clearInterval(id);
-  } else {
-   positionOfLiftTop[liftNumber]--;
-   lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
-  }
- }
- liftPresentFloor = floorNumber;
+    let id = setInterval(frame, 5);
+    function frame() {
+        if (positionOfLiftTop[liftNumber] <= top) {
+            clearInterval(id);
+        } else {
+            positionOfLiftTop[liftNumber]--;
+            lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
+        }
+    }
+    liftPresentFloor[liftNumber] = floorNumber;
 
 }
 
 function moveLiftDown(floorNumber, liftNumber) {
- let lift1 = document.getElementById(liftNumber);
- let floorA = offset(document.getElementById("floor" + floorNumber));
- let top = floorA.top;
- let left = floorA.left;
 
- console.log(top);
- console.log(left);
+    let lift1 = document.getElementById(liftNumber);
+    liftNumber = liftNumber.match(/\d/g);
+    liftNumber = liftNumber.join("");
+    let x = document.getElementById('liftSetup' + liftNumber);
+    let adjustTop = offset(x).top;
+    let floorA = offset(document.getElementById("floor" + floorNumber));
+    let top = floorA.top + adjustTop - 45;
+    let left = floorA.left;
+    console.log(top, left);
 
- liftNumber = liftNumber.match(/\d/g);
- liftNumber = liftNumber.join("");
+    if (liftPresentFloor[liftNumber] != floorNumber) {
+        let id = setInterval(frameUp, 5);
+        function frameUp() {
+            if (positionOfLiftTop[liftNumber] <= top) {
+                clearInterval(id);
+            } else {
+                positionOfLiftTop[liftNumber]--;
+                lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
+            }
+        }
 
- if (liftPresentFloor != floorNumber) {
-  let id = setInterval(frameUp, 10);
-  function frameUp() {
-   if (positionOfLiftTop[liftNumber] <= top) {
-    clearInterval(id);
-   } else {
-    positionOfLiftTop[liftNumber]--;
-    lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
-   }
-  }
-
- }
+    }
 
 
- let id = setInterval(frame, 10);
- function frame() {
-  if (positionOfLiftTop[liftNumber] >= top) {
-   clearInterval(id);
-  } else {
-   positionOfLiftTop[liftNumber]++;
-   lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
-  }
- }
- liftPresentFloor = floorNumber;
+    let id = setInterval(frame, 5);
+    function frame() {
+        if (positionOfLiftTop[liftNumber] >= top) {
+            clearInterval(id);
+        } else {
+            positionOfLiftTop[liftNumber]++;
+            lift1.style.top = positionOfLiftTop[liftNumber] + 'px';
+        }
+    }
+    liftPresentFloor[liftNumber] = floorNumber;
 
 }
 
